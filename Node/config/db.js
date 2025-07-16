ae6ff2e87ec58f7d8e5d0b72aab840c4195da496
“Tejas9420190282” <tejasshimpi877@gmail.com>
2025-07-16 15:28:20 +0530

    database connection

--- Node/config/db.js
+++ Node/config/db.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require("mongoose");
 const connectDB = async () => {
     try {
         
-        await mongoose.connect('mongodb://127.0.0.1:27017/test')
+        await mongoose.connect('mongodb://127.0.0.1:27017/read_File')
   
         console.log(`Database conneced successfully`.bgGreen);   
 
